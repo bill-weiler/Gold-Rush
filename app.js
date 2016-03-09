2b81dcbe23e391346485b690f3eeff17e26f2fe7
Kyle Miller <miller.kwill@gmail.com>
2016-03-08 19:23:40 -0700

    changed getMarker function and added array

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
 angular.module('GoldRushApp', [])
 
 //create the controller
-  .controller.('markerController', ['$scope', '$event', '$log', markerController])
+  .controller('markerController', ['$scope', markerController])
 
 //set mainController function
-function markerController ($scope, $event, $log){
-  $scope.getCoordinates = function(){
-    
+function markerController ($scope){
+// declaring array to store marker objects
+  var markerArr = []
+
+  function markerFactory (x, y){
+    this.coordx = x
+    this.coordy = y
+  }
+
+
+  $scope.getMarker = function(){
 //use of clientx/y to provide coordinates
-    var x = event.clientX;
-    var y = event.clientY;
-    $log.log(x + y)
-    var coor = (x + ', ' + y)
-    coordinateArr.push()
+    var x = MouseEvent.clientX;
+    var y = MouseEvent.clientY;
+    // var coor = (x + ', ' + y) no longer necessary because we can push direct info
+    markerArr.push(markerFactory(x, y))
   }
 }
