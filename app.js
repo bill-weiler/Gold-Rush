dd70344ce3e19b68301b36bf2ffaf6a6fdfc87fb
Student <Student@Mac-mini-2.local>
2016-03-10 10:49:12 -0700

    commit2

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ angular.module('GoldRushApp', [])
 
 //set mainController function
 function markerController ($scope, $event, $log){
+
+//declare coordinate array
+var coordinateArr = []
+
+//set callback function
   $scope.getCoordinates = function(){
-    
+
 //use of clientx/y to provide coordinates
-    var x = event.clientX;
-    var y = event.clientY;
-    $log.log(x + y)
+    var x = event.clientX
+    var y = event.clientY
+    $log.log(x + ', ' + y)
     var coor = (x + ', ' + y)
-    coordinateArr.push()
+    coordinateArr.push(coor)
   }
 }
+
+
+//make constructor function to create key/value pairs of coordinates
